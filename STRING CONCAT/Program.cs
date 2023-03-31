3ad09978866acc6c8da6ffe4cecc2e1bedb7048d
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-31 15:39:17 +0530

    Add project files.

---
+++ STRING CONCAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace STRING_CONCAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "manju ";
+            string lastName = "gelam";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+        }
+    }
+}
